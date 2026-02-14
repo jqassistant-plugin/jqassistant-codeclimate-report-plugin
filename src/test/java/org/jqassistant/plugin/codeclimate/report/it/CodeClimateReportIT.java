c3a5f1cc8c1b1cbc3573a8018461982af67c3264
Dirk Mahler <dirk.mahler@buschmais.com>
2026-02-14 20:37:38 +0100

    added IT

--- src/test/java/org/jqassistant/plugin/codeclimate/report/it/CodeClimateReportIT.java
+++ src/test/java/org/jqassistant/plugin/codeclimate/report/it/CodeClimateReportIT.java
@@ -1,6 +1,7 @@
 package org.jqassistant.plugin.codeclimate.report.it;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.Map;
 
 import com.buschmais.jqassistant.core.report.api.model.Result;
@@ -8,15 +9,18 @@ import com.buschmais.jqassistant.core.rule.api.model.Constraint;
 import com.buschmais.jqassistant.core.rule.api.model.RuleException;
 import com.buschmais.jqassistant.plugin.java.test.AbstractJavaPluginIT;
 
+import org.apache.commons.io.IOUtils;
 import org.junit.jupiter.api.Test;
 
 import static com.buschmais.jqassistant.core.report.api.model.Result.Status.FAILURE;
+import static java.nio.charset.StandardCharsets.UTF_8;
 import static org.assertj.core.api.Assertions.assertThat;
+import static uk.org.webcompere.modelassert.json.JsonAssertions.assertJson;
 
 public class CodeClimateReportIT extends AbstractJavaPluginIT {
 
     @Test
-    void constraintWithIssues() throws RuleException {
+    void constraintWithIssues() throws RuleException, IOException {
         scanClassPathDirectory(getClassesDirectory(TypeWithIssues.class));
         Result<Constraint> result = validateConstraint("codeclimate-report-it:ConstraintWithIssues", Map.of("fqn", TypeWithIssues.class.getName()));
 
@@ -24,6 +28,7 @@ public class CodeClimateReportIT extends AbstractJavaPluginIT {
 
         File codeClimateReport = new File("target/jqassistant/report/codeclimate/jqassistant-codeclimate.json");
         assertThat(codeClimateReport).exists();
+        String expectedJson = IOUtils.toString(CodeClimateReportIT.class.getResourceAsStream("/reference/constraintWithIssues.json"), UTF_8);
+        assertJson(codeClimateReport).isEqualTo(expectedJson);
     }
-
 }
